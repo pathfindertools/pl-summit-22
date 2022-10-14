e4863d50f9f54b35c10698ab627a5a3287ab7e8f
Tom Benbow <tom@hellomoon.net>
2022-10-14 13:48:57 -0500

    Add agenda and venues

--- components/blocks/tailwind-cards.tsx
+++ components/blocks/tailwind-cards.tsx
@@ -51,49 +51,51 @@ export const TailwindCards = ({ data, parentField = "" }) => {
   return (
     <Section className={tw.section} background={data.background} navigationLabel={data.navigationLabel}>
       <div className={tw.background}></div>
-      <div className={tw.contentWrap}>
-        <div className={tw.content}>
-          {data.label &&<h4 className={tw.label} data-tinafield={`${parentField}.label`}>{data.label}</h4>}
-          {data.headline && <h2 className={tw.headline} data-tinafield={`${parentField}.headline`}>{data.headline}</h2>}
-          {data.subhead && <h3 className={tw.subhead} data-tinafield={`${parentField}.subhead`}>{data.subhead}</h3>}
-          {data.body?.children && (
-            <div className={tw.text} data-tinafield={`${parentField}.body`}>
-              <TinaMarkdown content={data.body} />
-            </div>
-          )}
-          {data.buttons && (
-            <div className={tw.buttons}>
-              {data.buttons &&
-                data.buttons.map(function (button, index) {
-                  const element = (
-                      <a
-                        className={tw.button}
-                        href={button.link}
-                        target={linkTarget(button.link)}
-                        key={index}
-                        data-tinafield={`${parentField}.buttons.${index}`}
-                      >
-                        { button.label }
-                      </a>
-                    );
-                  return element;
-                })}
-            </div>
+      <div className={tw.wrap}>
+        <div className={tw.contentWrap}>
+          <div className={tw.content}>
+            {data.label &&<h4 className={tw.label} data-tinafield={`${parentField}.label`}>{data.label}</h4>}
+            {data.headline && <h2 className={tw.headline} data-tinafield={`${parentField}.headline`}>{data.headline}</h2>}
+            {data.subhead && <h3 className={tw.subhead} data-tinafield={`${parentField}.subhead`}>{data.subhead}</h3>}
+            {data.body?.children && (
+              <div className={tw.text} data-tinafield={`${parentField}.body`}>
+                <TinaMarkdown content={data.body} />
+              </div>
+            )}
+            {data.buttons && (
+              <div className={tw.buttons}>
+                {data.buttons &&
+                  data.buttons.map(function (button, index) {
+                    const element = (
+                        <a
+                          className={tw.button}
+                          href={button.link}
+                          target={linkTarget(button.link)}
+                          key={index}
+                          data-tinafield={`${parentField}.buttons.${index}`}
+                        >
+                          { button.label }
+                        </a>
+                      );
+                    return element;
+                  })}
+              </div>
+            )}
+          </div>
+        </div>
+        <div className={tw.cardWrap}>
+          {data.items && (
+            data.items.map(function (block, index) {
+              return <Card
+                key={index}
+                index={index}
+                data={block}
+                tw={data.tailwind || {}}
+                parentField={`${parentField}.items`}
+              />
+            })
           )}
         </div>
-      </div>
-      <div className={tw.cardWrap}>
-        {data.items && (
-          data.items.map(function (block, index) {
-            return <Card
-              key={index}
-              index={index}
-              data={block}
-              tw={data.tailwind || {}}
-              parentField={`${parentField}.items`}
-            />
-          })
-        )}
       </div>
     </Section>
   );
--- content/pages/index.md
+++ content/pages/index.md
@@ -36,6 +36,116 @@ blocks:
         link: 'https://github.com/pathfindertools/microgen'
         type: secondary
     _template: feature
+  - tailwind:
+      section: ''
+      wrap: max-w-site-full mx-auto p-20
+      imageWrap: ''
+      image: ''
+      contentWrap: ''
+      content: ''
+      label: ''
+      headline: ''
+      subhead: ''
+      text: ''
+      buttons: ''
+      button: ''
+      card: relative flex gap-8 items-center mb-8
+      cardImageWrap: w-5/12
+      cardImage: rounded-lg h-full
+      cardContentWrap: w-7/12
+      cardContent: text-white
+      cardLabel: >-
+        absolute z-20 px-8 left-0 top-1/2 transform -translate-y-1/2 w-5/12
+        text-center font-1 font-bold text-xl text-accent1 
+      cardHeadline: text-white text-4xl font-1 font-bold
+      cardSubhead: text-accent2 text-lg font-2 mb-6
+      cardText: text-white text-lg
+    background:
+      fillStyles: bg-accent1 opacity-100
+    label: ''
+    headline: ''
+    subhead: ''
+    body: ''
+    items:
+      - image:
+          src: >-
+            https://res.cloudinary.com/protocolai/image/upload/v1665676294/pl-summit-22/event-pl-summit_nmmypj.jpg
+        label: PL Summit at Convento de Beato
+        headline: PL Summit
+        subhead: 'October 24, 11am - 3pm'
+        text: >
+          ​The Protocol Labs Network will meet at Convento do Beato to hear Juan
+          Benet's vision for the future of the network, learn more about the
+          progress and impact being made by network teams, and prepare for a
+          fun-filled week of learning, collaboration, and connection.
+      - image:
+          src: >-
+            https://res.cloudinary.com/protocolai/image/upload/v1665676289/pl-summit-22/event-opening-party_o9s5zi.jpg
+        label: Opening Party at Estufa Fria
+        headline: Opening Party
+        subhead: 'October 24, 7pm - 11pm'
+        text: >
+          In the evening, join the Protocol Labs Network at the beautiful Estufa
+          Fria for the Lab Week opening party. We'll have cocktails, light
+          bites, and music to kick the week off in style.
+    _template: tailwindCards
+  - tailwind:
+      section: ''
+      wrap: max-w-site-full mx-auto px-20
+      imageWrap: ''
+      image: ''
+      contentWrap: max-w-site-1/2 mx-auto
+      content: ''
+      label: ''
+      headline: text-white text-6xl font-1 font-bold text-center mb-10
+      subhead: ''
+      text: ''
+      buttons: ''
+      button: ''
+      cardWrap: max-w-site-1/2 mx-auto text-center pb-6
+      card: mb-10
+      cardContent: ''
+      cardHeadline: font-2 text-accent2 font-bold text-xl
+      cardText: text-white text-lg font-1
+    background:
+      fillStyles: bg-accent1 opacity-100
+    label: ''
+    headline: Venues
+    subhead: ''
+    body: ''
+    items:
+      - headline: Convento do Beato
+        subhead: ''
+        text: |
+          Alameda do Beato 40, 1950-042 Lisboa, Portugal
+      - headline: Estufa Fria
+        subhead: ''
+        text: |
+          Parque Eduardo VII, 1070-051 Lisboa, Portugal
+    _template: tailwindCards
+  - tailwind:
+      section: ''
+      wrap: max-w-site-full px-20 mx-auto pb-28
+      imageWrap: ''
+      image: ''
+      contentWrap: ''
+      content: ''
+      label: ''
+      headline: ''
+      subhead: ''
+      text: ''
+      buttons: ''
+    image:
+      src: >-
+        https://res.cloudinary.com/protocolai/image/upload/v1665672987/pl-summit-22/map_dk6w44.png
+    background:
+      fillStyles: bg-accent1 opacity-100
+      src: ''
+    label: ''
+    headline: ''
+    subhead: ''
+    body: ''
+    _template: tailwindFeature
 meta:
   pageTitle: Microgen
   pageDescription: Make modern web 3.0 ready websites with a real-time visual editor.
