189590fc154ba994b730d1ca4e0fd63682d09b88
Tom Benbow <tom@hellomoon.net>
2022-10-17 09:35:42 -0500

    Disable branch switcher

--- .tina/schema.ts
+++ .tina/schema.ts
@@ -90,7 +90,7 @@ export const tinaConfig = defineConfig({
     /**
      * Enables experimental branch switcher
      */
-    cms.flags.set("branch-switcher", true);
+    // cms.flags.set("branch-switcher", true);
 
     /**
      * When `tina-admin` is enabled, this plugin configures contextual editing for collections
